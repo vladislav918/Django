d7b6611a29082d320c15bf0418e8f2ccd63f1ff9
vladislav918 <lvlal5759@gmail.com>
2024-02-23 02:49:20 +0700

    social link

--- main/models.py
+++ main/models.py
@@ -2,13 +2,28 @@ from django.db import models
 
 
 class Address(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    """ Класс модели адресов магазинов """
+    name = models.CharField(max_length=100, unique=True, verbose_name='Адрес магазина')
     latitude = models.FloatField(verbose_name='Широта')
     longitude = models.FloatField(verbose_name='Долгота')
 
     class Meta:
         verbose_name = 'Адрес магазина'
         verbose_name_plural = 'Адреса магазинов'
+
+    def __str__(self):
+        return self.name
+
+
+class Social(models.Model):
+    """ Класс модели соц. сетей страницы о нас """
+    icon = models.FileField(upload_to="icons/")
+    name = models.CharField(max_length=200)
+    link = models.URLField()
+
+    class Meta:
+        verbose_name = 'Социальные сети'
+        verbose_name_plural = 'Социальные сети'
 
     def __str__(self):
         return self.name
---
+++ main/templatetags/social_tags.py
@@ -0,0 +1,11 @@
+from django import template
+from main.models import Social
+
+
+register = template.Library()
+
+
+@register.simple_tag()
+def get_social_links():
+    """Вывод ссылок соц. сетей"""
+    return Social.objects.all()
