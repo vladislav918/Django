69ac497ef5e1bf709c3adecfbccb24dfce5fe862
vladislav918 <lvlal5759@gmail.com>
2024-05-07 01:04:45 +0700

    create webhook stripe

--- payment/urls.py
+++ payment/urls.py
@@ -6,9 +6,12 @@ from .views import (
     SuccessView,
 )
 
+from .webhooks import stripe_webhook
+
 
 urlpatterns = [
     path('process/', CreateCheckoutSessionView.as_view(), name='process'),
     path('cancel/', CancelView.as_view(), name='cancel'),
     path('success/', SuccessView.as_view(), name='success'),
+    path('webhook/', stripe_webhook, name='stripe-webhook'),
 ]
---
+++ payment/webhooks.py
@@ -0,0 +1,33 @@
+import stripe
+
+from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+from django.http import HttpResponse
+
+from orders.models import Order
+
+
+@csrf_exempt
+def stripe_webhook(request):
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload,
+            sig_header,
+            settings.STRIPE_WEBHOOK_SECRET)
+    except ValueError as e:
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        return HttpResponse(status=400)
+
+    if event['type'] == 'checkout.session.completed':
+        session = event['data']['object']
+        order_id = session['metadata']['order_id']
+        order = Order.objects.get(id=order_id)
+        order.paid = True
+        order.save()
+
+    return HttpResponse(status=200)
